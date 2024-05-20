6aae61f9a651dd2b96e64072f5533e2ba6a993e7
Serhii Voitovych <serhii.voitovych@ffw.com>
2024-05-20 15:11:46 +0200

    More prettier fixes

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
-const nextJest = require('next/jest')
+const nextJest = require('next/jest');
 
 /** @type {import('jest').Config} */
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
 // Add any custom config to be passed to Jest
 const config = {
